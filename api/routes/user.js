649e39e157fda25def44f481f1ebe33f101273b2
Vishweswarann <vishwesn@gmail.com>
2026-05-02 19:40:15 +0530

    Fix casing in authMiddleware import

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const auth = require('../middleware/authMiddleware'); // Protects these routes
+const auth = require('../middleware/authMiddleWare'); // Protects these routes
 
 // GET /api/user/stats - Fetch the user's data for the Dashboard
 router.get('/stats', auth, async (req, res) => {
